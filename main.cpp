aa01b7406e2cbfbe15dc00f11908384bb4636da9
Wendy Donis <wdonisluna1@student.rccd.edu>
2023-01-11 23:57:06 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: donis
+ * Created on January 11, 2023, 10:41 PM
+ * Purpose: To find the Sum of Two Numbers
+ *          and store the sum of these two 
+ *          numbers in a variable named total
+ */
+
+//System Libraries
+#include <iostream> ///Input Output Library
+using namespace std;
+
+//User Libraries
+ 
+ //Global constants not variables
+ //Science, Math, Conversions, Dimensions
+
+//Function Prototypes
+
+//Execution begins here at main
+int main ()
+{
+    int total= 50 + 100;
+    //total = 150
+    
+    //Initialize Variables
+    cout<<"The sum is" <<endl;
+    cout<< total <<endl;
+    
+    //Exit the Program
+    return 0;
+}
+
